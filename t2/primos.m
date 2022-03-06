ecff37a281bdd451a58e55512c447a6bc77dfbaf
Guilherme de Lima <gmendoncalima@hotmail.com>
2022-03-06 00:14:02 -0300

    subs

--- t2/primos.m
+++ t2/primos.m
@@ -1,3 +1,3 @@
-primes = sieve[2..50] ||uma lista que recebe uma função de condições para o numero ser primo
+primos = sieve[2..50] ||uma lista que recebe uma função de condições para o numero ser primo
 sieve (p:x) = p : sieve [n | n <- x; n mod p ~= 0]
-test=lay (map show primes)
+teste=lay (map show primos)
