787569637b74b00771c89a1be96c3fc94215b518
Guilherme de Lima <gmendoncalima@hotmail.com>
2022-04-04 21:09:27 -0300

    ex3.2.2

---
+++ ex3.2.2/listaenc.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+typedef struct list{
+	int c;
+	struct list *prox;
+	}list; //Criamos uma struct que será a nossa lista encadeada
+
+int main(){
+	struct list *origem=NULL;//criaremos nossa origem que servirá como cabeça de nossa lista
+	list *A=(list*) malloc(sizeof(list*));
+	A->c=1;
+	A->prox=origem;//A irá aponta pra onde a origem aponta(Nulo)
+	origem=A;//origem irá apontar pra A
+	
+	list *B=(list*) malloc(sizeof(list*));
+	B->c=2;
+	B->prox=A->prox;//B irá aponta pra onde A aponta(Nulo)
+	A->prox=B;//A irá apontar para B
+	list *C=(list*) malloc(sizeof(list*));
+	C->c=3;
+	C->prox=B->prox;
+	B->prox=C;
+	list *D=(list*) malloc(sizeof(list*));
+	D->c=4;
+	D->prox=C->prox;
+	C->prox=D;
+	
+	return 0;
+}
+
