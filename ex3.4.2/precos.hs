ae97d46a39b31cd0d217ae41ed5e16f592c019d5
Guilherme de Lima <gmendoncalima@hotmail.com>
2022-04-13 22:06:55 -0300

    ex3.4.2

---
+++ ex3.4.2/precos.hs
@@ -0,0 +1,12 @@
+import Data.List
+import Graphics.Rendering.OpenGL (VariableType(Float'))
+
+impostoRPRJ::Float->Float 
+impostoRPRJ(x) = x+(x*(15/100)) --imposto sobre roupas
+
+precoNovo=map impostoRPRJ [55.90,39.15,100,74.50]
+main = do
+    
+    print(precoNovo) --mostrará o novo preço dos produtos de acordo com a nova politica de imposto
+    print(filter (<60.00) precoNovo) --o preço dos produtos que estão abaixo da media de inflação
+    print(foldr max 0 precoNovo) --o produto mais caro da loja
