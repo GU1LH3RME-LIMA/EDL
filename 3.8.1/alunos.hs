a61b947f20bc97abab8541810fa9f9abfb921c62
Guilherme de Lima <gmendoncalima@hotmail.com>
2022-04-29 21:08:26 -0300

    alunos aprovados

---
+++ 3.8.1/alunos.hs
@@ -0,0 +1,41 @@
+import Text.Printf (printf)
+import Text.PrettyPrint.Annotated.HughesPJClass (Pretty(pPrintList))
+
+type Aluno = (String,Float ,Float)
+--criado um tipo aluno
+
+nome :: Aluno -> String
+nome (x,_,_) = x
+
+p1 :: Aluno -> Float 
+p1 (_,y,_) = y
+
+p2 :: Aluno -> Float 
+p2 (_,_,z) = z
+
+media_aluno :: Aluno -> Float
+media_aluno (x, y, z) = ( y + z) / 2
+--calcula a média dos alunos
+
+
+alunos_abaixo :: Aluno -> String
+alunos_abaixo(x,y,z) 
+    |media_aluno(x,y,z) >=7 =x
+    |otherwise=""
+
+alunos::[Aluno]
+alunos=[("Crzinho",4.0,5.5),("Mbappe",8.7,6.3),("Samidion Ammin",6.9,10),("Arrascaeta",10.0,10.0),("Diego Lopes",5.9,4.0),("Yago",5.4,4.3),("Alex Sena",7,7),("Raquel",7.5,9),("Miguel",2,9),("Alexandre",8,4)]
+--  criado os alunos na escola
+
+media_alunos :: [Float]
+media_alunos = map media_aluno alunos
+--a media dos alunos
+main = do
+    --print alunos
+    --print(map nome alunos) -- mapeado o nome dos alunos
+    print media_alunos -- mostrará as médias
+    print (map alunos_abaixo alunos)
+    
+    
+
+
